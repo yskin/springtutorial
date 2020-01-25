8d2972cfc6e6ca1506ee044ce890b02a0da5c805
yskin <7m7110123@gmail.com>
2020-01-25 17:40:32 +0900

    no message

---
+++ jackson-databinding-json-demo/src/main/com/luv2code/jackson/json/demo/Driver.java
@@ -0,0 +1,13 @@
+package com.luv2code.jackson.json.demo;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
